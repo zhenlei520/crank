7237ed8cf86d54a1151c8aa7733ad1adda87a011
zhenlei520 <wangzhenlei520@gmail.com>
2021-12-16 00:09:41 +0800

    chore: wrk2

--- samples/hello/Startup.cs
+++ samples/hello/Startup.cs
@@ -2,9 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
